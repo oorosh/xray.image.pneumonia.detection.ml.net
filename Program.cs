5177b6e947f0081678591c918924cc45ecfc820c
Uros <b.urosh@gmail.com>
2020-04-26 19:15:39 +0200

    small refactor

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.ML;
-using Microsoft.ML.Transforms;
 using Microsoft.ML.Vision;
-using pneumonia.detection.common;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -18,37 +15,35 @@ namespace pneumonia.detection
             const string datasetUrl = "https://storage.googleapis.com/kaggle-data-sets/17810/23812/bundle/archive.zip?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1587921765&Signature=HxSUJTlJVYahLDVTXsjq0q15M0RdUlhVeAAAcHHDgfO15r9T9vNrDbVQdac%2BZ7vn5FAJJLxRaOlWUVRQIcf5P2fwF0D6vM3mMdB76x6aukiFu6kSRYL8XWbM2Kw8aPjazhmvSnVJpRTFAlycpfh7kqU2g%2F8kEEgo9De4lU7hDZ85RtjQfajPxpLmgdrOlxTbGmm8fQaZ4DJgqDpU00cLKQAJSTFsBraM3xZcZMFu4tf6GKyUXSq%2FFR08h%2BOkQuUOL5xFI2aLweFSgKGMxfDawjXqNK5O3yqaLtDVapEMbT%2BzopDQeC0XabTysK6J4aktdrMhnSLh0E2MKSpzpCBBmg%3D%3D&response-content-disposition=attachment%3B+filename%3Dchest-xray-pneumonia.zip";
             const string datasetFullName = "chest-xray-pneumonia.zip";
 
-            var projectDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../"));     
-            
+            var projectDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../"));
+
             var datasetFolder = Path.Combine(projectDirectory, "dataset");
 
-            //Download data set and unzip it
-            DownloadImageSet(datasetFolder, datasetUrl, datasetFullName);
+            // 1. Download the image set and unzip
+            Utility.DownloadImageSet(datasetFolder, datasetUrl, datasetFullName);
 
             // Measuring training time
             var watch = Stopwatch.StartNew();
 
             var workspaceRelativePath = Path.Combine(projectDirectory, "workspace");
-            var trainImageFolderPath = Path.Combine(datasetFolder, "chest_xray", "train");
-            var testImageFolderPath = Path.Combine(datasetFolder, "chest_xray", "test");
-            var validationImageFolderPath = Path.Combine(datasetFolder, "chest_xray", "val");
-            
-            var trainImages = LoadImagesFromDirectory(trainImageFolderPath, true);
+
+            datasetFolder = Path.Combine(projectDirectory, "chest_xray");
+            var trainImageFolderPath = Path.Combine(datasetFolder, "train");
+            var testImageFolderPath = Path.Combine(datasetFolder, "test");
+            var validationImageFolderPath = Path.Combine(datasetFolder, "val");
 
             var context = new MLContext(seed: 1);
 
-            var trainImageData = context.Data.LoadFromEnumerable(trainImages);
-            var trainImageDataShuffle = context.Data.ShuffleRows(trainImageData);
+            // 2. Load the initial full image-set into an IDataView and shuffle so it'll be better balanced
+            var trainImageDataShuffle = Utility.GetTrainImageData(trainImageFolderPath, context);
 
-            // Apply transforms to the input dataset:
-            // MapValueToKey : map 'string' type labels to keys
-            // LoadImages : load raw images to "Image" column
-            trainImageDataShuffle = TransformDataView(context, trainImageDataShuffle, trainImageFolderPath);
+            // 3. Load Images with in-memory type within the IDataView and Transform Labels to Keys (Categorical)
+            trainImageDataShuffle = Utility.TransformDataView(context, trainImageDataShuffle, trainImageFolderPath);
 
-            var testImages = LoadImagesFromDirectory(testImageFolderPath, true);
+            // Load test images from files to memory
+            var testImages = Utility.LoadImagesFromDirectory(testImageFolderPath, true);
             var testImageDataView = context.Data.LoadFromEnumerable(testImages);
-
-            testImageDataView = TransformDataView(context, testImageDataView, testImageFolderPath);            
+            testImageDataView = Utility.TransformDataView(context, testImageDataView, testImageFolderPath);
 
             var classifierOptions = new ImageClassificationTrainer.Options()
             {
@@ -64,11 +59,13 @@ namespace pneumonia.detection
                 MetricsCallback = (metrics) => Console.WriteLine(metrics),
                 ValidationSet = testImageDataView,
                 // Disable EarlyStopping to run to specified number of epochs.
+                //EarlyStoppingCriteria = null,
                 ReuseTrainSetBottleneckCachedValues = true,
-                EarlyStoppingCriteria = null,
-                WorkspacePath = workspaceRelativePath                
+                WorkspacePath = workspaceRelativePath
             };
 
+
+            //4. Define the model's training pipeline using DNN default values
             var pipeline = context.MulticlassClassification.Trainers
                .ImageClassification(classifierOptions)
                .Append(context.Transforms.Conversion.MapKeyToValue(
@@ -79,25 +76,21 @@ namespace pneumonia.detection
               "with DNN Transfer Learning on top of the selected " +
               "pre-trained model/architecture ***");
 
+            //5. Train
             var model = pipeline.Fit(trainImageDataShuffle);
 
             Console.WriteLine("Training with transfer learning finished.");           
-
-            // Evaluate the trained model on the passed test dataset.
-            Console.WriteLine("Making bulk predictions and evaluating model's " +
-               "quality...");
-
             watch.Stop();
             var elapsedMs = watch.ElapsedMilliseconds;
 
-            Console.WriteLine($"Training with transfer learning took: {(elapsedMs / 1000)/60} minutes");
+            Console.WriteLine($"Training with transfer learning took: {(elapsedMs / 1000) / 60} minutes.");
 
-            context.Model.Save(model, trainImageDataShuffle.Schema, "model.zip");
-
-            var valImages = LoadImagesFromDirectory(validationImageFolderPath, true);
+            //6. Evaluate the trained model on the passed test dataset.
+            Console.WriteLine("Evaluate model with validation data.\n");
+            var valImages = Utility.LoadImagesFromDirectory(validationImageFolderPath, true);
             var valImageDataView = context.Data.LoadFromEnumerable(valImages);
 
-            valImageDataView = TransformDataView(context, valImageDataView, validationImageFolderPath);
+            valImageDataView = Utility.TransformDataView(context, valImageDataView, validationImageFolderPath);
 
             var metric = context.MulticlassClassification.Evaluate(model.Transform(valImageDataView));
 
@@ -105,30 +98,9 @@ namespace pneumonia.detection
                 $"MacroAccuracy : " + metric.MacroAccuracy + "\n" +
                 $"MicroAccuracy : " + metric.MicroAccuracy + "\n");
 
+            context.Model.Save(model, trainImageDataShuffle.Schema, "model.zip");
+
             Console.ReadLine();
         }
-
-        public static IDataView TransformDataView(MLContext context, IDataView dataView, string imageFolderPath)
-        {
-            return context.Transforms.Conversion
-               .MapValueToKey("Label", keyOrdinality: ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
-               .Append(context.Transforms.LoadRawImageBytes("Image", imageFolderPath, "ImagePath"))
-               .Fit(dataView)
-               .Transform(dataView);
-        }
-
-        public static string DownloadImageSet(string imagesDownloadFolder, string datasetUrl, string datasetFullName)
-        {
-            // get a set of images to teach the network about the new classes
-            Web.Download(datasetUrl, imagesDownloadFolder, datasetFullName);
-
-            Compress.UnZip(Path.Join(imagesDownloadFolder, datasetFullName), imagesDownloadFolder);
-
-            return Path.GetFileNameWithoutExtension(datasetFullName);
-        }
-
-        public static IEnumerable<ImageData> LoadImagesFromDirectory(string folder,bool useFolderNameAsLabel = true)
-           => FileUtils.LoadImagesFromDirectory(folder, useFolderNameAsLabel)
-               .Select(x => new ImageData { ImagePath = x.imagePath, Label = x.label });
     }
 }
---
+++ Utility.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using Microsoft.ML.Transforms;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+using Microsoft.ML;
+using System.IO;
+using System.Linq;
+
+namespace pneumonia.detection
+{
+    public static class Utility
+    {
+        public static IDataView GetTrainImageData(string trainImageFolderPath, MLContext context)
+        {
+            var trainImages = LoadImagesFromDirectory(trainImageFolderPath, true);
+            var trainImageData = context.Data.LoadFromEnumerable(trainImages);
+            var trainImageDataShuffle = context.Data.ShuffleRows(trainImageData);
+            return trainImageDataShuffle;
+        }
+
+        public static IDataView TransformDataView(MLContext context, IDataView dataView, string imageFolderPath)
+        {
+            // Apply transforms to the input dataset:
+            // MapValueToKey : map 'string' type labels to keys
+            // LoadImages : load raw images to "Image" column
+            return context.Transforms.Conversion
+               .MapValueToKey("Label", keyOrdinality: ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
+               .Append(context.Transforms.LoadRawImageBytes("Image", imageFolderPath, "ImagePath"))
+               .Fit(dataView)
+               .Transform(dataView);
+        }
+
+        public static string DownloadImageSet(string imagesDownloadFolder, string datasetUrl, string datasetFullName)
+        {
+            // get a set of images to teach the network about the new classes
+            Download(datasetUrl, imagesDownloadFolder, datasetFullName);
+
+            UnZip(Path.Join(imagesDownloadFolder, datasetFullName), imagesDownloadFolder);
+
+            return Path.GetFileNameWithoutExtension(datasetFullName);
+        }        
+
+        public static bool Download(string url, string destDir, string destFileName)
+        {
+            if (destFileName == null)
+                destFileName = url.Split(Path.DirectorySeparatorChar).Last();
+
+            Directory.CreateDirectory(destDir);
+
+            string relativeFilePath = Path.Combine(destDir, destFileName);
+
+            if (File.Exists(relativeFilePath))
+            {
+                Console.WriteLine($"{relativeFilePath} already exists.");
+                return false;
+            }
+
+            var wc = new WebClient();
+            Console.WriteLine($"Downloading {relativeFilePath}");
+            var download = Task.Run(() => wc.DownloadFile(url, relativeFilePath));
+            while (!download.IsCompleted)
+            {
+                Thread.Sleep(1000);
+                Console.Write(".");
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"Downloaded {relativeFilePath}");
+
+            return true;
+        }
+
+        public static void ExtractGZip(string gzipFileName, string targetDir)
+        {
+            // Use a 4K buffer. Any larger is a waste.    
+            byte[] dataBuffer = new byte[4096];
+
+            using (System.IO.Stream fs = new FileStream(gzipFileName, FileMode.Open, FileAccess.Read))
+            {
+                using (GZipInputStream gzipStream = new GZipInputStream(fs))
+                {
+                    // Change this to your needs
+                    string fnOut = Path.Combine(targetDir, Path.GetFileNameWithoutExtension(gzipFileName));
+
+                    using (FileStream fsOut = File.Create(fnOut))
+                    {
+                        StreamUtils.Copy(gzipStream, fsOut, dataBuffer);
+                    }
+                }
+            }
+        }
+
+        public static void UnZip(String gzArchiveName, String destFolder)
+        {
+            var flag = gzArchiveName.Split(Path.DirectorySeparatorChar).Last().Split('.').First() + ".bin";
+            if (File.Exists(Path.Combine(destFolder, flag))) return;
+
+            Console.WriteLine($"Extracting.");
+            var task = Task.Run(() =>
+            {
+                ZipFile.ExtractToDirectory(gzArchiveName, destFolder);
+            });
+
+            while (!task.IsCompleted)
+            {
+                Thread.Sleep(200);
+                Console.Write(".");
+            }
+
+            File.Create(Path.Combine(destFolder, flag));
+            Console.WriteLine("");
+            Console.WriteLine("Extracting is completed.");
+        }
+
+        public static void ExtractTGZ(String gzArchiveName, String destFolder)
+        {
+            var flag = gzArchiveName.Split(Path.DirectorySeparatorChar).Last().Split('.').First() + ".bin";
+            if (File.Exists(Path.Combine(destFolder, flag))) return;
+
+            Console.WriteLine($"Extracting.");
+            var task = Task.Run(() =>
+            {
+                using (var inStream = File.OpenRead(gzArchiveName))
+                {
+                    using (var gzipStream = new GZipInputStream(inStream))
+                    {
+                        using (TarArchive tarArchive = TarArchive.CreateInputTarArchive(gzipStream))
+                            tarArchive.ExtractContents(destFolder);
+                    }
+                }
+            });
+
+            while (!task.IsCompleted)
+            {
+                Thread.Sleep(200);
+                Console.Write(".");
+            }
+
+            File.Create(Path.Combine(destFolder, flag));
+            Console.WriteLine("");
+            Console.WriteLine("Extracting is completed.");
+        }
+
+        public static IEnumerable<ImageData> LoadImagesFromDirectory(string folder, bool useFolderNameAsLabel = true)
+           => LoadImagesFromFileSystem(folder, useFolderNameAsLabel)
+               .Select(x => new ImageData { ImagePath = x.imagePath, Label = x.label });
+
+        public static IEnumerable<(string imagePath, string label)> LoadImagesFromFileSystem(string folder,bool useFolderNameasLabel)
+        {
+            var imagesPath = Directory.GetFiles(folder, "*", searchOption: SearchOption.AllDirectories);
+
+            return useFolderNameasLabel
+                ? imagesPath.Select(imagePath => (imagePath, Directory.GetParent(imagePath).Name))
+                : imagesPath.Select(imagePath =>
+                {
+                    var label = Path.GetFileName(imagePath);
+                    for (var index = 0; index < label.Length; index++)
+                    {
+                        if (!char.IsLetter(label[index]))
+                        {
+                            label = label.Substring(0, index);
+                            break;
+                        }
+                    }
+                    return (imagePath, label);
+                });
+        }
+    }
+}
